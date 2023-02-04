919ebc5c95a3f532fa4286bf7ff73919079c4f51
Seth-Callaghan <zhap055@live.rhul.ac.uk>
2023-02-04 12:31:45 +0000

    Added a test solution that runs on .net 6

--- backend/LetMeKnowAPI/ApiTests/Tests.cs
+++ backend/LetMeKnowAPI/ApiTests/Tests.cs
@@ -9,7 +9,7 @@ namespace ApiTests
         [Test]
         public void Test1()
         {
-            Assert.True(true);
+            
         }
     }
 }
---
+++ backend/LetMeKnowAPI/Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using LetMeKnowAPI;
+using LetMeKnowAPI.Controllers;
+
+namespace Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        
+    }
+}
