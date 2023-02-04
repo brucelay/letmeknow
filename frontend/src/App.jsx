9828d72873b7d4f029138e1017185b7933a7ccb3
Bruce Lay <zhac057@live.rhul.ac.uk>
2023-02-04 18:00:06 +0000

    Frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <>
+      <div class="flex flex-wrap">
+        <div class="basis-full">
+          <h1>LetMeKnow</h1>
+        </div>
+        <div class="basis-1/2">
+          container1
+        </div>
+        <div class="basis-1/2">
+          container2
+        </div>
+      </div>
+    </>
+  )
+}
