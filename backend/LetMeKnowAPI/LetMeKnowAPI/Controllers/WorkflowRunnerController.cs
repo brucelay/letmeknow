db6543569df4286b8c36771c9d27783886fc707c
Seth-Callaghan <zhap055@live.rhul.ac.uk>
2023-02-04 12:56:13 +0000

    added a method to text a user given the correct info

--- backend/LetMeKnowAPI/LetMeKnowAPI/Controllers/WorkflowRunnerController.cs
+++ backend/LetMeKnowAPI/LetMeKnowAPI/Controllers/WorkflowRunnerController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
 
 namespace LetMeKnowAPI.Controllers
 {
@@ -12,8 +16,18 @@ namespace LetMeKnowAPI.Controllers
         {
             _logger = logger;
         }
-        
-        
-        
+
+
+        [HttpPost]
+        public HttpStatusCode Text(string targetPhoneNumber, string outgoingPhoneNumber, string SID, string AuthToken, string OutMessage)
+        {
+            TwilioClient.Init(SID, AuthToken);
+            
+            var message = MessageResource.Create(
+                to: new PhoneNumber(targetPhoneNumber), // Replace with your recipient's phone number
+                from: new PhoneNumber(outgoingPhoneNumber), // Replace with a Twilio-provided phone number
+                body: OutMessage);
+            return HttpStatusCode.OK;
+        }
     }
 }
