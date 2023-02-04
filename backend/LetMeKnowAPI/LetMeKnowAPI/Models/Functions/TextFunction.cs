1b41284f6d1ebf0999927d05e2ced89e35c9f132
Bruce Lay <zhac057@live.rhul.ac.uk>
2023-02-04 17:00:12 +0000

    TextFunction: remove IResult usage

--- backend/LetMeKnowAPI/LetMeKnowAPI/Models/Functions/TextFunction.cs
+++ backend/LetMeKnowAPI/LetMeKnowAPI/Models/Functions/TextFunction.cs
@@ -1,14 +1,43 @@
 namespace LetMeKnowAPI.Models.Functions;
 
-public class TextFunction : IWorkflowFunction<ITextResult>
+using System.Net;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+public class TextFunction : IWorkflowFunction<TextResult>
 {
+    private string _targetPhoneNumber;
+    private string _outgoingPhoneNumber;
+    private readonly string _OutMessage;
+    private readonly string _SID;
+    private readonly string _AuthToken;
+
+    private TextResult _textResult;
+    
+    public TextFunction(string targetPhoneNumber, string outgoingPhoneNumber, string outMessage, string sid, string authToken)
+    {
+        _targetPhoneNumber = targetPhoneNumber;
+        _outgoingPhoneNumber = outgoingPhoneNumber;
+        _OutMessage = outMessage;
+        _SID = sid;
+        _AuthToken = authToken;
+    }
+    
     public void RunFunction()
     {
-        throw new NotImplementedException();
+        TwilioClient.Init(_SID, _AuthToken);
+            
+        var message = MessageResource.Create(
+            to: new PhoneNumber(_targetPhoneNumber), // Replace with your recipient's phone number
+            from: new PhoneNumber(_outgoingPhoneNumber), // Replace with a Twilio-provided phone number
+            body: _OutMessage);
+        
+        _textResult = new TextResult(true);
     }
 
-    public IResult<ITextResult> GetResult()
+    public TextResult GetResult()
     {
-        throw new NotImplementedException();
+        return _textResult;
     }
 }
