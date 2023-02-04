295216adad6d194fae89aec9429c114d5d92d4a3
Seth-Callaghan <zhap055@live.rhul.ac.uk>
2023-02-04 11:40:48 +0000

    Created new controller, for sending text messages

---
+++ backend/LetMeKnowAPI/LetMeKnowAPI/Controllers/WorkflowRunnerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LetMeKnowAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WorkflowRunnerController
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WorkflowRunnerController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        
+        
+        
+    }
+}
