080facc66da7a7851927436992807fabfc01f56b
Seth Callaghan <sethcallaghan2013@gmail.com>
2023-02-04 23:15:28 +0000

    added scheduling of multipletext messages

--- backend/LetMeKnowAPI/LetMeKnowAPI/Controllers/WorkflowController.cs
+++ backend/LetMeKnowAPI/LetMeKnowAPI/Controllers/WorkflowController.cs
@@ -13,12 +13,30 @@ namespace LetMeKnowAPI.Controllers;
 [Route("[controller]")]
 public class WorkflowController : ControllerBase
 {
-    [HttpPost("run")]
-    public async Task<bool> RunWorkflow([FromBody] dynamic jsonData)
+    
+    [HttpPost("CreateWorkFlow")]
+    public async Task<OkResult> RunWorkflow([FromBody] dynamic jsonData)
     {
         JArray data = JsonConvert.DeserializeObject<dynamic>(jsonData.ToString());
         var workFlowRunner = new WorkflowRunner(data);
         await workFlowRunner.Execute();
-        return true;
+        return Ok();
+    }
+    
+    [HttpPost("CreateEvent")]
+    public Task<OkResult> CreateNewWorkFlowEvent([FromBody] dynamic jsonData, float freqInMins, int amount)
+    {
+        JArray data = JsonConvert.DeserializeObject<dynamic>(jsonData.ToString());
+        var workflowRunner = new WorkflowRunner(data);
+        ScheduleWorkFlow(workflowRunner, TimeSpan.FromMinutes(freqInMins), amount);
+        return Task.FromResult(Ok());
+    }
+    private async void ScheduleWorkFlow(WorkflowRunner workflow, TimeSpan interval, int amountOfTimes)
+    {
+        for (int i = 0; i < amountOfTimes; i++)
+        {
+            await Task.Delay(interval);
+            await workflow.Execute();
+        }
     }
 }
