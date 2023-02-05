dc5c638b7efb28857bfd043ee602bec48cbb6cc7
Bruce Lay <zhac057@live.rhul.ac.uk>
2023-02-05 09:21:36 +0000

    Phone number input at top

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import WorkflowBuilder from "./components/WorkflowBuilder";
 
 export default function App() {
+  [number, setNumber] = createSignal("")
+
   return (
     <>
       <div class="flex flex-col h-full">
         <div class="basis-2/12 p-5 ml-2 mt-2 font-bold shadow" >
           <h1 id="title" class="text-7xl table-header-group">LetMe<span class="text-orange-600">Know</span>,</h1>
+          <div class="mt-5">
+            <input onInput={(e) => setNumber(e.target.value)} class="text-5xl rounded" type="text" placeholder="+4407192874981"></input>
+          </div>
         </div>
-
-        <WorkflowBuilder />
+        <WorkflowBuilder number={number}/>
       </div>
     </>
   );
